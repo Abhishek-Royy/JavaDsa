6ded018ec681af0ab04526593c135105bb15c5d5
Abhishek-coder2001 <abhishek800.roy@gmail.com>
2023-11-10 11:46:33 +0530

    dsa files upload

--- string.java
+++ string.java
@@ -1,14 +1,61 @@
+import java.util.*;
+
 public class string {
     public static void main(String[] args) {
 
         // METHOD OF STRING.
 
-        String name="Abhishek";
-        // charAt Method.
-       for(int i=0;i<name.length();i++){
-        System.out.println(name.charAt(i));
-       }
+        /*
+         * String name="Abhishek";
+         * // charAt Method.
+         * for(int i=0;i<name.length();i++){
+         * System.out.println(name.charAt(i));
+         * }
+         */
+
+        // compare two string
+        /*
+         * String name1="zbhishek";
+         * String name2="sbhishek";
+         * if(name1.compareTo(name2)==0){
+         * System.out.println("Both are equal");
+         * }
+         * else if(name1.compareTo(name2)<0){
+         * System.out.println("-ve");
+         * }
+         * else{
+         * System.out.println("+ve");
+         * }
+         */
+
+        // subString
+        /*
+         * String sentence="Hello guys wellcome channel";
+         * System.out.println(sentence.substring(20,27));
+         */
+
+
+        /*   //Q. user input a email and get username from this email, write a programe on the basis of question ?
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter email: ");
+        String email = sc.nextLine();
+
+        String username = "";
+
+        for (int i = 0; i <= email.length(); i++) {
+            if (email.charAt(i) == '@') {
+                break;
+            } else {
+                username += email.charAt(i);
+            }
+        }
+
+        System.out.println(username);
+        */
+
+
         
+
     }
-    
+
 }
